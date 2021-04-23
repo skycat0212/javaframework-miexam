48a73c3c6ea559a53ca33195f5d3b96835fcb4a0
skycat0212 <mr_magnet@naver.com>
2021-04-23 18:09:20 +0900

    13번 문제 #13

--- java-framework-class-miexam/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ java-framework-class-miexam/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,24 +1,44 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.*;
 
 public class UserDao {
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcContext;
 
-    public UserDao(JdbcContext jdbcContext){
+    public UserDao(JdbcTemplate jdbcContext){
         this.jdbcContext = jdbcContext;
     }
 
     public User get(Integer id) throws SQLException {
         String sql = "select * from userinfo2 where id = ?";
         Object[] params = new Object[]{ id };
-        return jdbcContext.get(sql, params);
+        return jdbcContext.query(sql, params, rs -> {
+            User user = null;
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            } return user;
+        });
     }
 
     public void insert(User user) throws SQLException {
         String sql = "insert into userinfo2 (name, password) values (?,?)";
         Object[] params = new Object[]{ user.getName(), user.getPassword() };
-        jdbcContext.insert(user, sql, params);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcContext.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for (int i = 0; i < params.length ; i ++) {
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        user.setId(keyHolder.getKey().intValue());
     }
 
     public void update(User user) throws SQLException {
